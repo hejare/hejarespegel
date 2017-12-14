1cf63729001260529399f9b49f9a006f5dcbd0ab
Henrik Svensson <henrik@hejare.se>
2017-12-14 10:28:17 +0100

    adds luddes sun animation

--- public/src/sun.js
+++ public/src/sun.js
@@ -1,48 +1,40 @@
 var gameHeight = 600;
 var gameWidth = 800;
 
-var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, 'hejare-spegel', { preload: preload, create: create });
 
 function preload() {
-
-    //  You can fill the preloader with as many assets as your game requires
-
     //  Here we are loading an image. The first parameter is the unique
     //  string by which we'll identify the image later in our code.
 
     //  The second parameter is the URL of the image (relative)
     game.load.spritesheet('explosion', 'img/explosion_1.png', 88, 93);
-
+    game.load.spritesheet('sun', 'img/sun.png', 182, 183, 24);
 }
 
 function create() {
+    // game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
-    var explosion = game.add.sprite(20, 20, 'explosion', 5);
-    var explode = explosion.animations.add('explode');
-    var tween = game.add.tween(explosion);
+    var sun = game.add.sprite(-400, -400, 'sun', 5);
+    var shine = sun.animations.add('shine');
+    shine.play(24, true);
 
-    // game.physics.enable(explosion, Phaser.Physics.ARCADE);
-    // explosion.body.velocity.x=0.1;
-    // explosion.body.velocity.y=0.1;
+    // Update the sun position once every minute.
+    moveSun();
+    setInterval(moveSun, 60000);
 
-    // tween.to({x:600, y:100}, 10000, 'Linear', true, 0);
+}
 
-    explode.play(24, true);
-
-    setInterval(function() {
-        var coordinates = sunPosition(gameWidth - explosion.width, 100);
-        explosion.x = coordinates.x;
-        explosion.y = coordinates.y;
-    }, 100);
-
+function moveSun() {
+    var coordinates = sunPosition(gameWidth - sun.width, 100);
+    sun.x = coordinates.x;
+    sun.y = coordinates.y;
 }
 
 function sunPosition(width, height) {
     var d = new Date();
-    // var x = (d.getHours() * 60 + d.getMinutes()) / (24 * 60);
-    var x = (d.getSeconds() * 1000 + d.getMilliseconds()) / 60000;
+    var x = (d.getHours() * 60 + d.getMinutes()) / (24 * 60);
+    // var x = (d.getSeconds() * 1000 + d.getMilliseconds()) / 60000;
     return {
         x: x * width,
         y: 4 * Math.pow((x - 0.5), 2) * height
